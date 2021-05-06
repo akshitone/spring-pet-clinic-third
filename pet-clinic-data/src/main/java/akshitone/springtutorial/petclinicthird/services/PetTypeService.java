150ec9e4a72f349c296981a5b69439f553f65c7f
akshitone <akshitone8@gmail.com>
2021-05-06 13:41:03 +0530

    Added pet type in data initializer

---
+++ pet-clinic-data/src/main/java/akshitone/springtutorial/petclinicthird/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package akshitone.springtutorial.petclinicthird.services;
+
+import akshitone.springtutorial.petclinicthird.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
--- pet-clinic-web/src/main/java/akshitone/springtutorial/petclinicthird/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/akshitone/springtutorial/petclinicthird/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package akshitone.springtutorial.petclinicthird.bootstrap;
 
 import akshitone.springtutorial.petclinicthird.model.Owner;
+import akshitone.springtutorial.petclinicthird.model.PetType;
 import akshitone.springtutorial.petclinicthird.model.Vet;
 import akshitone.springtutorial.petclinicthird.services.OwnerService;
+import akshitone.springtutorial.petclinicthird.services.PetTypeService;
 import akshitone.springtutorial.petclinicthird.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,26 @@ import org.springframework.stereotype.Component;
 public class DataInitializer implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded pet types data...");
+
         Owner lucifer = new Owner();
         lucifer.setFirstName("Lucifer");
         lucifer.setLastName("Morningstar");
